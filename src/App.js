cc8542517df4a15d658a32d15610c89352a52f46
Zhekun Zhang <zhekunz2@Zhekuns-MacBook-Pro.local>
2019-10-20 22:04:00 -0500

    home page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+            <div className="flexslider js-fullheight">
+                <ul className="slides">
+                <li style={{backgroundImage: 'url(images/img_bg_1.jpg)'}}>
+                    <div className="overlay" />
+                    <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                        <div className="slider-text-inner js-fullheight">
+                            <div className="desc">
+                            <h1>Hi! <br />I'm Jackson</h1>
+                            <h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank">colorlib.com</a></h2>
+                            <p><a className="btn btn-primary btn-learn">Download CV <em className="icon-download4" /></a></p>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </li>
+                <li style={{backgroundImage: 'url(images/img_bg_2.jpg)'}}>
+                    <div className="overlay" />
+                    <div className="container-fluid">
+                    <div className="row">
+                        <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                        <div className="slider-text-inner">
+                            <div className="desc">
+                            <h1>I am <br />a Designer</h1>
+                            <h2>100% html5 bootstrap templates Made by <a href="https://colorlib.com/" target="_blank">colorlib.com</a></h2>
+                            <p><a className="btn btn-primary btn-learn">View Portfolio <em className="icon-briefcase3" /></a></p>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </li>
+                </ul>
+            </div>
+        </section>
+      </div>
+    )
+  }
 }
-
-export default App;
