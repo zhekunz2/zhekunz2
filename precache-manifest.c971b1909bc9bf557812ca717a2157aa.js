4dd652de14bf3289c30e613b55e85604c3137f42
Zhekun Zhang <zhekunz2@Zhekuns-MacBook-Pro.local>
2019-10-20 22:16:16 -0500

    Updates

--- precache-manifest.c971b1909bc9bf557812ca717a2157aa.js
+++ precache-manifest.c971b1909bc9bf557812ca717a2157aa.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b9d2c911b01fcbbbad56e1305ab954ec",
+    "revision": "2c0c6e45f333db5ad56ea0764af11b34",
     "url": "/zhekunz2/index.html"
   },
   {
-    "revision": "f6dd07a24f1648190c69",
-    "url": "/zhekunz2/static/css/main.b100e6da.chunk.css"
+    "revision": "f0f4671ff4b444bfe467",
+    "url": "/zhekunz2/static/css/main.4c8934a1.chunk.css"
   },
   {
     "revision": "d62b92e0596331306551",
     "url": "/zhekunz2/static/js/2.31307e90.chunk.js"
   },
   {
-    "revision": "f6dd07a24f1648190c69",
+    "revision": "f0f4671ff4b444bfe467",
     "url": "/zhekunz2/static/js/main.f1fb6451.chunk.js"
   },
   {
